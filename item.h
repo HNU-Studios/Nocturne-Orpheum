14cdeac84e36cbbb8bfc0bfcc3502042fc842e49
Aarav <aarav25803691@gmail.com>
2025-01-30 09:42:53 -0600

    Add equip, improve saving, add stats
    Add equip function, save items using item.h instead of string for easier access & add some stats

--- item.h
+++ item.h
@@ -1,19 +1,27 @@
 #include <string>
+#include <vector>
 using std::string;
 class item{
     string name;
     string type;
+    map<string, int> buffs;
     int power;
     public:
         item(string n, string t, int p):
             name(n), type(t), power(p) {}
-        string getName(){
+        string getName() const{
             return name;
         }
-        string getType(){
+        string getType() const{
             return type;
         }
-        int getPower(){
+        int getPower() const{
             return power;
+        }
+        void buffs(string stat, int amount){
+            buffs[stat] = amount;
+        }
+        int getBuff(string stat){
+            return buffs[stat];
         }
 };
--- main.cpp
+++ main.cpp
@@ -6,31 +6,32 @@
 #include "item.h"
 using namespace std;
 // Inventory arrays
-map<string, int> food;
-map<string, int> weapons;
-map<string, int> gear;
-map<string, int> stat = {{"Strength", 1}, {"Defense", 1}};
+map<item, int> food;
+map<item, int> weapons;
+map<item, int> gear;
+map<string, int> stat = {{"Strength", 1}, {"Defense", 1}, {"Intelligence", 1}, {"HP", 10}};
 map<string, string> skills;
+map<string, string> equipped = {{"Weapon", ""}, {"Helmet", ""}, {"Breastplate", ""}, {"Gloves", ""}, {"Mask", ""}, {"Artifact 1", ""}, {"Artifact 2", ""}, {"Artifact 3", ""}, {"Artifact 4", ""}, {"Artifact 5", ""}};
 vector<item> ground;
 int c;
 void inventory(){ // Functions for printing a user's inventory
-    /*Instructions*/ cout << "For food: f\nFor weapons: w\nFor gear: g\nFor skills: s\nEnter your option (leave blank to exit this menu): ";
+    /*Instructions*/ cout << "For food: f\nFor weapons: w\nFor gear: g\nFor skills: s\nFor all items: i\nEnter your option (leave blank to exit this menu): ";
     char option;
     cin >> option;
     // Printing individual inventory menus
     if (option == 'f'){
         for (const auto& pair : food) {
-            cout << pair.first << " x" << pair.second << "\n";
+            cout << pair.first.getName() << " x" << pair.second << "\n";
         }
     }
     else if (option == 'w'){
         for (const auto& pair : weapons) {
-            cout << pair.first << " x" << pair.second << "\n";
+            cout << pair.first.getName() << " x" << pair.second << "\n";
         }
     }
     else if (option == 'g'){
         for (const auto& pair : gear) {
-            cout << pair.first << " x" << pair.second << "\n";
+            cout << pair.first.getName() << " x" << pair.second << "\n";
         }
     }
     else if (option == 's'){
@@ -42,15 +43,15 @@ void inventory(){ // Functions for printing a user's inventory
     else if(option == 'i'){
         cout << "ALL FOOD:\n";
         for (const auto& pair : food) {
-            cout << pair.first << " x" << pair.second << "\n";
+            cout << pair.first.getName() << " x" << pair.second << "\n";
         }
         cout << "ALL WEAPONS:\n";
         for (const auto& pair : weapons) {
-            cout << pair.first << " x" << pair.second << "\n";
+            cout << pair.first.getName() << " x" << pair.second << "\n";
         }
         cout << "ALL GEAR:\n";
         for (const auto& pair : gear) {
-            cout << pair.first << " x" << pair.second << "\n";
+            cout << pair.first.getName() << " x" << pair.second << "\n";
         }
         cout << "ALL SKILLS:\n";
         for (const auto& pair : skills) {
@@ -95,19 +96,19 @@ void pick(){ // Picks up an item from the ground
     if (choice == 0) return;
     item selected = ground[choice - 1];
     string itemType = selected.getType();
-    if (ground[choice - 1].getType() == "weapon"){
-        if (weapons.find(selected.getName()) == weapons.end()) weapons[selected.getName()] = 1;
-        else weapons[selected.getName()]++;
+    if (ground[choice - 1].getType() == "Weapon"){
+        if (weapons.find(selected) == weapons.end()) weapons[selected] = 1;
+        else weapons[selected]++;
         cout << "Picked up weapon: " << selected.getName() << "\n";
     }
-    else if (ground[choice - 1].getType() == "food"){
-        if (food.find(selected.getName()) == food.end()) food[selected.getName()] = 1;
-        else food[selected.getName()]++;
+    else if (ground[choice - 1].getType() == "Food"){
+        if (food.find(selected) == food.end()) food[selected] = 1;
+        else food[selected]++;
         cout << "Picked up food: " << selected.getName() << "\n";
     }
-    else if (ground[choice - 1].getType() == "gear"){
-        if (gear.find(selected.getName()) == gear.end()) gear[selected.getName()] = 1;
-        else gear[selected.getName()]++;
+    else if (ground[choice - 1].getType() == "Artifact" || ground[choice - 1].getType() == "Helmet" || ground[choice - 1].getType() == "Breastplate" || ground[choice - 1].getType() == "Mask" || ground[choice - 1].getType() == "Gloves"){
+        if (gear.find(selected) == gear.end()) gear[selected] = 1;
+        else gear[selected]++;
         cout << "Picked up gear: " << selected.getName() << "\n";
     }
     ground.erase(ground.begin() + (choice - 1));
@@ -122,13 +123,116 @@ void pick(){ // Picks up an item from the ground
         cout << "Type p again to pick up another item.\n";
     }
 }
-
+void equip(){ // Lets the user equip an item
+    item toEquip = item("", "", 0);
+    c = 1;
+    cout << "Please select a category of item to equip\n\n";
+    cout << "For weapons: w\nFor gear/ artifacts: g\nEnter your option (leave blank to exit this menu): ";
+    char option;
+    cin >> option;
+    if (option == 'w'){
+        for (const auto& pair : weapons) {
+            cout << c << ": " << pair.first.getName() << " x" << pair.second << "\n";
+            c++;
+        }
+        int o;
+        cout << "Enter an item's number";
+        while (true) {
+            cout << "Enter the number of the item you want to pick up (0 to leave): ";
+            if (!(cin >> o)) {
+                // If the input is not an integer, clear the error & ignore invalid input
+                cin.clear(); // Clear error
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore rest of invalid input
+                cout << "Invalid input. Please enter a valid number.\n";
+                continue; // Continue the code
+            }
+            // cin >> choice;
+            if (o == 0) break;
+            else if (o < 1 || o > weapons.size()) { // Runs if choice isn't valid
+                cout << "Invalid choice. Please select a valid item number.\n";
+            }
+            else break;
+        }
+        auto it = next(weapons.begin(), o - 1); 
+        toEquip = it->first;
+        if (toEquip.getType() == "Weapon"){ 
+            equipped["Weapon"] = toEquip.getName(); 
+            stat["Strength"] += toEquip.getPower();
+    }
+    }
+    else if (option == 'g'){
+        for (const auto& pair : gear) {
+            cout << c << ": " << pair.first.getName() << " x" << pair.second << "\n";
+            c++;
+        }
+        int o;
+        cout << "Enter an item's number";
+        int o;
+        cout << "Enter an item's number";
+        while (true) {
+            cout << "Enter the number of the item you want to pick up (0 to leave): ";
+            if (!(cin >> o)) {
+                // If the input is not an integer, clear the error & ignore invalid input
+                cin.clear(); // Clear error
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore rest of invalid input
+                cout << "Invalid input. Please enter a valid number.\n";
+                continue; // Continue the code
+            }
+            // cin >> choice;
+            if (o == 0) break;
+            else if (o < 1 || o > gear.size()) { // Runs if choice isn't valid
+                cout << "Invalid choice. Please select a valid item number.\n";
+            }
+            else break;
+        }
+        auto it = next(gear.begin(), o - 1);
+        toEquip = it->first;
+        if (toEquip.getType() == "Helmet"){
+            equipped["Helmet"] = toEquip.getName(); 
+            stat["HP"] += toEquip.getPower();
+        }
+        else if (toEquip.getType() == "Breastplate"){
+            equipped["Breastplate"] = toEquip.getName(); 
+            stat["HP"] += toEquip.getPower();
+        }
+        else if (toEquip.getType() == "Gloves"){
+            equipped["Gloves"] = toEquip.getName(); 
+            stat["Strength"] += toEquip.getPower();
+        }
+        else if (toEquip.getType() == "Mask"){
+            equipped["Mask"] = toEquip.getName(); 
+            stat["HP"] += toEquip.getPower();
+        }
+        // Checks for artifact slots and adds to the lowest one. If full, warns the user.
+        else if (toEquip.getType() == "Artifact"){
+            if (equipped["Artifact 5"] != ""){
+                cout << "You have too many artifacts equipped (Max 5).";
+                return;
+            }
+            else {
+                if (equipped["Artifact 4"] != ""){
+                    equipped["Artifact 5"] = toEquip.getName();
+                }
+                else {
+                    if (equipped["Artifact 3"] != ""){
+                        equipped["Artifact 4"] = toEquip.getName();
+                    }
+                    else{
+                        if (equipped["Artifact 2"] != ""){
+                            equipped["Artifact 1"] = toEquip.getName();
+                        }
+                    }
+                }
+            }
+        }
+    } 
+}
 
 int main(){
     cout << "Welcome, adventurer.\nEnter your name here: ";
     string name;
     cin >> name;
-    cout << "Hello, " << name << ". Welcome to the world. You start as a human with all your stats set to one, but as time goes on, you can\nlevel up your stats, learn skills, collect weapons, and find gear. Gear and weapons can also have their\nown buffs and skills as you get further in the game. Here, take this [BASIC DULL SWORD] (press p to pick up).\n";
+    cout << "Hello, " << name << ". Welcome to the world. You start as a human with all your stats set to 1, HP at 10, but as time goes\non, you can level up your stats, learn skills, collect weapons, and find gear. Gear and weapons can also have their\nown buffs and skills as you get further in the game. Here, take this [BASIC DULL SWORD] (press p to pick up).\n";
     ground.push_back(item("Dull sword", "weapon", 1));
     while (true){
         char option;
@@ -138,7 +242,9 @@ int main(){
         else if (option == 'h' || option == '?') help();
         else if (option == 's') stats();
         else if (option == 'p') pick();
+        else if (option == 'e') equip();
         else if (option == 'q') break;
+        else continue;
     }
     return 0;
 }
