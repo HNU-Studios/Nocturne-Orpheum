8ff97e37420cd072688af367c31508dbe8c69c0c
probablyAarav <aarav25803691@gmail.com>
2025-02-04 08:57:55 -0600

    adding first sector

--- sector.h
+++ sector.h
@@ -4,6 +4,8 @@ class sector{
     string Name;
     vector<item> Ground;
     int MinLevel;
+    bool in;
     public:
-        sector(string n, vector<item> g, int level): Name(n), Ground(g), MinLevel(level){};
+        sector(string n, vector<item> g, int level, bool In): Name(n), Ground(g), MinLevel(level), in(In){};
+        sector(string n, vector<item> g, int level): Name(n), Ground(g), MinLevel(level), in(false){};
 };
--- sectors.h
+++ sectors.h
@@ -1,2 +1,4 @@
 #include "sector.h"
 using namespace std;
+vector<item> emptyGround;
+sector Andris = sector("Andris", emptyGround, 1, true);
