25f2d558c5c9084df86c7bb8ea9abdb9122e7102
aarav yadav <aarav25803691@gmail.com>
2025-02-24 23:15:00 -0600

    fixes

--- enemy.h
+++ enemy.h
@@ -1,16 +1,18 @@
 #include <string>
 #include <map>
-// using namespace std;
+#include "item.h"
 class enemy {
     std::string name;
     int speed;
     int power;
-    // std::map<std::string, int> buffs;
+    std::map<std::string, int> loot;
+    int health;
 public:
-    enemy(std::string n, int s, int p): name(n), speed(s), power(p) {};
-    // enemy(std::string n, int s, int p, std::map<std::string, int> b): name(n), speed(s), power(p), buffs(b) {};
+    enemy(std::string n, int s, int p): name(n), speed(s), power(p), loot({{"Gold", 5}, {"EXP", 50}}), health(5) {};
+    enemy(std::string n, int s, int p, std::map<std::string, int> l): name(n), speed(s), power(p), loot(l), health(5) {};
+    enemy(std::string n, int s, int p, std::map<std::string, int> l, int h): name(n), speed(s), power(p), loot(l), health(h) {};
     string getName(){return name;}
     int getSpeed(){return speed;}
     int getPower(){return power;}
-    // std::map<std::string, int> getBuffs(){return buffs;}
+    int getLoot(item i){}
 };
