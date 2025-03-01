445ce62341501bede2bd3f9056f97a32147848f7
aarav yadav <aarav25803691@gmail.com>
2025-03-01 12:53:02 -0600

    editing sector.h

--- sector.h
+++ sector.h
@@ -6,6 +6,10 @@ class sector{
     int MinLevel;
     // bool in;
     int size;
+    sector north(string, vector<item> ground, int);
+    sector south(string, vector<item> ground, int);
+    sector east(string, vector<item> ground, int);
+    sector west(string, vector<item> ground, int);
     public:
         // sector(string n, vector<item> g, int level, bool In): Name(n), Ground(g), MinLevel(level), in(In){};
         sector(string n, vector<item> g, int level): Name(n), Ground(g), MinLevel(level), size(5)/*, in(false)*/{};
